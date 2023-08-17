459171cb01e62342b1a33270935901c8be8df4b9
Karan <karan@noesis.tech>
2023-08-17 09:35:47 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function initializeWidget(){
+    var mainContainer = document.createElement('div');
+    mainContainer.id = 'main-container';
+
+    let iframe = document.createElement('iframe');
+    iframe.id = 'iframe-container';
+    iframe.src = "../page2.html";
+
+    mainContainer.appendChild(iframe);
+
+    document.body.appendChild(mainContainer);
+
+    let frame = document.querySelector("#iframe-container");
+
+    frame.addEventListener('load', function(){
+        console.log("loaded")
+        frame.contentWindow.postMessage("demo", window.location.origin);
+    })
+}
+
+window.addEventListener('DOMContentLoaded', function(){
+    initializeWidget();
+})
